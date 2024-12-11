b4a3c62b16ce08e161702293b34388547a527e83
YosaAE24 <cebastin2424@gmail.com>
2024-12-11 13:51:11 +0700

    mengumpulkan komponen dalam folder award kedalam halaman award

---
+++ src/pages/Award.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AwardsPage from "../awards/AwardsPage";
+import TestimoniJuri from "../awards/TestimoniJuri";
+
+function Award() {
+    return (
+        <div>
+            <AwardsPage />
+            <TestimoniJuri />
+        </div>
+    )
+}
+
+export default Award;
